02a4ed1fcfab37cbd9842ba3ffad226ddd2e5dc4
TanmayShil <tanmayshil2002@gmail.com>
2024-06-10 11:09:22 +0530

    edit popup completed

--- src/Admin/customer/report/reportDetails.js
+++ src/Admin/customer/report/reportDetails.js
@@ -7,16 +7,30 @@ import {
   CardContent,
   Stack,
   IconButton,
+  Box,
+  styled,
 } from "@mui/material";
 import StickyNote2Icon from "@mui/icons-material/StickyNote2";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import EditIcon from "@mui/icons-material/Edit";
 import CloseIcon from "@mui/icons-material/Close";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogTitle from "@mui/material/DialogTitle";
 
+const VisuallyHiddenInput = styled("input")({
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
+  height: 1,
+  overflow: "hidden",
+  position: "absolute",
+  bottom: 0,
+  left: 0,
+  whiteSpace: "nowrap",
+  width: 1,
+});
 const reportTypes = [
   "Preservation certificate",
   "CFU & sterility report",
@@ -94,6 +108,153 @@ const ReportDetails = () => {
                     <div
                       style={{
                         border: "1px solid #000",
+                        width: "300px",
+                        height: "300px",
+                        margin: "0 auto",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      <div
+                        style={{
+                          border: "1px solid #000",
+                          width: "270px",
+                          height: "270px",
+                          margin: "0 auto",
+                          display: "flex",
+                          justifyContent: "center",
+                          alignItems: "center",
+                        }}
+                      >
+                        {/* Image content will go here */}
+                      </div>
+                    </div>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      sx={{
+                        marginTop: "20px",
+                        borderRadius: "30px",
+                      }}
+                    >
+                      Done
+                    </Button>
+                  </div>
+                </DialogContent>
+              </Dialog>
+              <Button
+                variant="contained"
+                color="primary"
+                sx={{
+                  borderRadius: "30px",
+                  background: "#cce0ff",
+                  color: "black",
+                  "&:hover": {
+                    background: "#cce0ff",
+                  },
+                }}
+                onClick={() => setOpenEdit(true)}
+              >
+                <EditIcon />
+                Edit
+              </Button>
+              <Dialog open={openEdit}>
+                <DialogContent sx={{ width: "500px" }}>
+                  <CloseIcon
+                    sx={{
+                      marginLeft: "400px",
+                      cursor: "pointer",
+                    }}
+                    onClick={() => setOpenEdit(!openEdit)}
+                  />
+                  <div style={{ textAlign: "center" }}>
+                    <h5>0012 - Customer name</h5>
+                    <h2>Edit/Upload File</h2>
+                    <h2>Maternity sample report</h2>
+                    <Box sx={{ marginBottom: "10px" }}>
+                      <Button
+                        component="label"
+                        role={undefined}
+                        variant="outlined"
+                        tabIndex={-1}
+                        startIcon={<CloudUploadIcon />}
+                        sx={{ padding: "20px" }}
+                      >
+                        Add File here
+                        <VisuallyHiddenInput type="file" />
+                      </Button>
+                    </Box>
+                    <div
+                      style={{
+                        border: "1px solid #000",
+                        width: "300px",
+                        height: "300px",
+                        margin: "0 auto",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      <div
+                        style={{
+                          border: "1px solid #000",
+                          width: "270px",
+                          height: "270px",
+                          margin: "0 auto",
+                          display: "flex",
+                          justifyContent: "center",
+                          alignItems: "center",
+                        }}
+                      >
+                        {/* Image content will go here */}
+                      </div>
+                    </div>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      sx={{
+                        marginTop: "20px",
+                        borderRadius: "30px",
+                      }}
+                    >
+                      Upload
+                    </Button>
+                  </div>
+                </DialogContent>
+              </Dialog>
+              <Button
+                variant="contained"
+                // color="secondary"
+                sx={{
+                  borderRadius: "30px",
+                  background: "#f0f0f0",
+                  color: "black",
+                  "&:hover": {
+                    background: "#f0f0f0",
+                  },
+                }}
+                onClick={() => setOpenDelete(true)}
+              >
+                <DeleteOutlineIcon />
+                Delete
+              </Button>
+              <Dialog open={openDelete}>
+                <DialogContent sx={{ width: "500px" }}>
+                  <CloseIcon
+                    sx={{
+                      marginLeft: "400px",
+                      cursor: "pointer",
+                    }}
+                    onClick={() => setOpenDelete(!openDelete)}
+                  />
+                  <div style={{ textAlign: "center" }}>
+                    <h5>0012 - Customer name</h5>
+                    <h2>View File</h2>
+                    <h2>Maternity sample report</h2>
+                    <div
+                      style={{
+                        border: "1px solid #000",
                         width: "80%",
                         height: "400px",
                         margin: "0 auto",
@@ -129,36 +290,6 @@ const ReportDetails = () => {
                   </div>
                 </DialogContent>
               </Dialog>
-              <Button
-                variant="contained"
-                color="primary"
-                sx={{
-                  borderRadius: "30px",
-                  background: "#cce0ff",
-                  color: "black",
-                  "&:hover": {
-                    background: "#cce0ff",
-                  },
-                }}
-              >
-                <EditIcon />
-                Edit
-              </Button>
-              <Button
-                variant="contained"
-                // color="secondary"
-                sx={{
-                  borderRadius: "30px",
-                  background: "#f0f0f0",
-                  color: "black",
-                  "&:hover": {
-                    background: "#f0f0f0",
-                  },
-                }}
-              >
-                <DeleteOutlineIcon />
-                Delete
-              </Button>
             </Stack>
           </CardContent>
         </Card>
