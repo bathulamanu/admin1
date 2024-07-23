a7dc0c5c877eb8eb41cef3c6a7c7110e5be72dab
tanPalipoint <tanmay.shil@palipoint.com>
2024-07-23 11:25:47 +0530

    changes api endpoint

--- src/utils/api/httpRequest.js
+++ src/utils/api/httpRequest.js
@@ -1,18 +1,18 @@
-import axios from 'axios'
-const loginUserDetails = localStorage.getItem('loginUser')
-const data = JSON.parse(loginUserDetails)
+import axios from "axios";
+const loginUserDetails = localStorage.getItem("loginUser");
+const data = JSON.parse(loginUserDetails);
 // const { token } = data;
 // console.log("dkjgksgdk", token);
 
 const api = axios.create({
-  baseURL: 'http://18.61.70.225:4000/flyingbyts/api/user',
+  baseURL: "http://18.61.158.245:4000/flyingbyts/api/user",
   //   withCredentials: true,
   headers: {
-    'Content-type': 'application/json',
-    Authorization: data?.token ? data?.token : '',
+    "Content-type": "application/json",
+    Authorization: data?.token ? data?.token : "",
   },
-})
+});
 
-export default api
+export default api;
 
 // console.log('accessToken', localStorage.getItem('accessToken'))
